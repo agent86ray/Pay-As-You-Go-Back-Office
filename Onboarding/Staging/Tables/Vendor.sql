941b066cb63776bc7732bd8419f7ceb4e75cab3e
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-08-04 08:10:18 -0400

    Create Load-Vendors SSIS package.

---
+++ Onboarding/Staging/Tables/Vendor.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [AP].[Vendor]
+(
+	[VendorId]			BIGINT NOT NULL 
+		CONSTRAINT PK_Vendor 
+			PRIMARY KEY CLUSTERED
+,	[VendorName]		VARCHAR(50) NOT NULL
+,	[BankRoutingNumber]	BIGINT NULL
+,	[BankAccountNumber]	BIGINT NULL
+,	[LoadDate]			SMALLDATETIME NOT NULL
+		CONSTRAINT DF_Vendor_LoadDate
+			DEFAULT GETDATE()
+)
