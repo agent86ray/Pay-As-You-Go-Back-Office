58ca828f8f01bd44b47be67f326c4dc98278e82b
Raymond Barley <raymondbarley@itresourcepartners.com>
2020-08-05 06:45:43 -0400

    Add LoadVendorsFromCSV stored procedure.

---
+++ Onboarding/Staging/Stored Procedures/LoadVendorsFromCSV.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [AP].[LoadVendorsFromCSV]
+	@P_CSV_FILE_NAME	VARCHAR(256)
+AS
+BEGIN
+	DECLARE
+		@EXECUTION_ID	BIGINT;
+
+	EXEC SSISDB.catalog.create_execution 
+		@folder_name = N''
+	,	@project_name = N''
+	,	@package_name = N''
+	,	@reference_id = 1
+	,	@execution_id = @EXECUTION_ID OUTPUT;
+END
